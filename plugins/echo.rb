4e3673b5660ff13bbe891816e1fa619c8552d234
Scott McCormack <flintinatux@gmail.com>
2012-11-06 22:29:13 -0500

    Added a goodbye matcher

--- plugins/echo.rb
+++ plugins/echo.rb
@@ -1,6 +1,7 @@
 class Echo < CampfireBot::Plugin
   on_command 'say', :say
   on_command 'echo', :say
+  on_message %r[hea(d|ded|ding|din') out], :goodbye
 
   def initialize
     @log = Logging.logger["CampfireBot::Plugin::Echo"]
@@ -8,5 +9,18 @@ class Echo < CampfireBot::Plugin
   
   def say(m)
     m.speak(m[:message])
+  end
+
+  def goodbye(m)
+    name = m[:person].split.first
+    farewells = [
+                  "Peace.",
+                  "Cya later, #{name}.",
+                  "See you tomorrow, #{name}!",
+                  "Goodbye, #{name}.",
+                  "Don't worry, #{name}. I'll be here when you get back!",
+                  "Drive fast and turn hard."
+                ]
+    m.speak farewells.sample
   end
 end
